a222bda2b0ce642d626398c58e11ab2ec424afd2
vhjihuang <1076891184@qq.com>
2024-01-22 15:20:49 +0800

    refactor(module/example|module/dynamic): 删除测试代码

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,21 +3,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContentModule } from './modules/content/content.module';
-import { ExampleModule } from './modules/example/example.module';
 import { CoreModule } from './modules/core/core.module';
-import { DynamicsModule } from './modules/dynamic/dynamic.module';
+import { DatabaseModule } from './modules/database/database.module';
 
 @Module({
-  imports: [
-    ContentModule,
-    ExampleModule,
-    CoreModule,
-    DynamicsModule.forRoot({
-      config: {
-        name: 'dynamic',
-      },
-    }),
-  ],
+  imports: [ContentModule, CoreModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -1,4 +1,4 @@
 import { Module } from '@nestjs/common';
 
 @Module({})
-export class CoreModule {}
+export class DatabaseModule {}
